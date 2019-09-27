952f246a48576b3c6957e3ee756ade13b1fc3299
Steven Massaro <steven.massaro.web@gmail.com>
2019-09-27 14:30:18 -0400

    convert to docker image build

--- src/main/java/musicclient/MusicClient.java
+++ src/main/java/musicclient/MusicClient.java
@@ -3,13 +3,12 @@ package musicclient;
 import musicclient.filters.BasicAuthorizationHeaderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @SpringBootApplication
-public class MusicClient extends SpringBootServletInitializer {
+public class MusicClient {
 
     public static void main(String[] args) {
         SpringApplication.run(MusicClient.class, args);
