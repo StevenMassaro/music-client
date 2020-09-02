01b989e6be141e16f70c48f97611549339979d9c
Steven Massaro <steven.massaro.web@gmail.com>
2020-09-02 18:13:36 -0400

    properly get credentials from jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -24,19 +24,20 @@ node {
         sh 'curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text="${JOB_BASE_NAME} - ${BUILD_NUMBER} finished" || true'
     }
     stage('GitHub mirror') {
-        dir('gh') {
-            cleanWs()
-            def scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
-            def mirrorUrl = "https://github.com/StevenMassaro/music-client.git"
+        cleanWs()
+        def scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
 
+        dir('gh') {
             sh 'git config --global credential.helper cache'
             sh "git config --global credential.helper 'cache --timeout=3600'"
             git credentialsId: 'GitBucketReadOnly', url: scmUrl
-            git credentialsId: 'GitHubStevenMassaro', url: mirrorUrl
 
             sh "git clone --bare ${scmUrl}"
             dir('music.git') {
-                sh "git push --mirror ${mirrorUrl}"
+                withCredentials([usernameColonPassword(credentialsId: 'GitHubStevenMassaro', variable: 'USERPASS')]) {
+                    def mirrorUrl = "https://${USERPASS}@github.com/StevenMassaro/music-client.git"
+                    sh "git push --mirror ${mirrorUrl}"
+                }
             }
         }
     }
