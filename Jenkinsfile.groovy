d834c6fdf682fd7c43f4a3e349e04bf9c23e7c59
Steven Massaro <steven.massaro.web@gmail.com>
2020-09-30 20:43:19 -0400

    simplify jenkins checkout

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,7 +3,7 @@ node {
     properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), disableConcurrentBuilds()])
     stage('Preparation') {
         sh 'curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text="${JOB_BASE_NAME} - ${BUILD_NUMBER} started" || true'
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'GitBucketReadOnly', url: 'http://192.168.0.117:8084/git/music/MusicClient.git']]])
+        checkout scm
         mvnHome = tool 'M3'
     }
     stage('Build') {
