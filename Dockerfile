7f97a1dc60d8073c7b5203dde524685446d7d508
Steven Massaro <steven.massaro.web@gmail.com>
2020-12-12 10:58:59 -0500

    build docker image in Jenkinsfile.groovy

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:13-alpine
 EXPOSE 8080
-ADD /target/music-client-ui.jar ui.jar
+ADD /music-client-ui/target/music-client-ui.jar ui.jar
 ENTRYPOINT ["java", "-jar", "ui.jar"]
--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,41 +1,59 @@
-node {
-    def mvnHome
-    properties([buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '10')), disableConcurrentBuilds()])
-    stage('Preparation') {
-        sh 'curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text="${JOB_BASE_NAME} - ${BUILD_NUMBER} started" || true'
-        checkout scm
-        mvnHome = tool 'M3'
+pipeline {
+    agent {
+        label 'master'
     }
-    stage('Build') {
-        sh 'echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin'
-        // Run the maven build
-        withEnv(["MVN_HOME=$mvnHome"]) {
-            if (isUnix()) {
-                sh '"$MVN_HOME/bin/mvn" clean deploy -P ui'
-            } else {
-                bat(/"%MVN_HOME%\bin\mvn" clean deploy -P ui/)
+
+    tools {
+        maven 'M3'
+    }
+
+    environment {
+        pom = readMavenPom().getVersion()
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                sh 'curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text="${JOB_BASE_NAME} - ${BUILD_NUMBER} started" || true'
+                sh 'mvn clean install -P ui'
             }
         }
-    }
-    stage('Results') {
-        //junit '**/target/surefire-reports/TEST-*.xml'
-        archiveArtifacts 'music-client-ui/target/*.jar'
-        sh 'curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text="${JOB_BASE_NAME} - ${BUILD_NUMBER} finished" || true'
-    }
-    stage('GitHub mirror') {
-        cleanWs()
-        def scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
+        stage('Docker') {
+            steps {
+                script {
+                    image = docker.build("stevenmassaro/music-client:latest")
+                    docker.withRegistry('', 'DockerHub') {
+                        image.push()
+                        image.push(pom)
+                    }
+                }
+            }
+        }
+        stage('Results') {
+            steps {
+                archiveArtifacts 'music-client-ui/target/*.jar'
+                sh 'curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID} -d text="${JOB_BASE_NAME} - ${BUILD_NUMBER} finished" || true'
+            }
+        }
+        stage('GitHub mirror'){
+            steps {
+                script {
+                    cleanWs()
+                    def scmUrl = scm.getUserRemoteConfigs()[0].getUrl()
 
-        dir('gh') {
-            sh 'git config --global credential.helper cache'
-            sh "git config --global credential.helper 'cache --timeout=3600'"
-            git credentialsId: 'GitBucketReadOnly', url: scmUrl
+                    dir('gh') {
+                        sh 'git config --global credential.helper cache'
+                        sh "git config --global credential.helper 'cache --timeout=3600'"
+                        git credentialsId: 'GitBucketReadOnly', url: scmUrl
 
-            sh "git clone --bare ${scmUrl}"
-            dir('music.git') {
-                withCredentials([usernameColonPassword(credentialsId: 'GitHubStevenMassaro', variable: 'USERPASS')]) {
-                    def mirrorUrl = "https://${USERPASS}@github.com/StevenMassaro/music-client.git"
-                    sh "git push --mirror ${mirrorUrl}"
+                        sh "git clone --bare ${scmUrl}"
+                        dir('music.git') {
+                            withCredentials([usernameColonPassword(credentialsId: 'GitHubStevenMassaro', variable: 'USERPASS')]) {
+                                def mirrorUrl = "https://${USERPASS}@github.com/StevenMassaro/music-client.git"
+                                sh "git push --mirror ${mirrorUrl}"
+                            }
+                        }
+                    }
                 }
             }
         }
